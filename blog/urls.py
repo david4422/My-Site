a082ae63b2d52471f8e604cc12e1bed9fcb33ccb
David Cohen <davidcohen6636@gmail.com>
2023-01-29 18:52:24 +0200

    stored-posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("" , views.Blog.as_view(), name="starting-page"),
     path("posts", views.BlogList.as_view(), name="posts-page"),
-    path("posts/<slug:slug>", views.BlogDetails.as_view(), name="details-page") #my-first-blog
+    path("posts/<slug:slug>", views.BlogDetails.as_view(), name="details-page"), #my-first-blog,
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 
 ]
